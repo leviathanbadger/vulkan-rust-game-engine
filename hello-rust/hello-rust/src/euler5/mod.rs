085236055b3fa4004a728d5304474d1bf0013659
Brandon Slade <brandonyoyoslade@gmail.com>
2022-10-18 17:05:26 -0600

    feat(euler): add solution for euler#5

---
+++ hello-rust/hello-rust/src/euler5/mod.rs
@@ -0,0 +1,30 @@
+
+
+fn smallest_multiple(n: i32) -> i64 {
+    assert!(n <= 20);
+
+    let mut smallest = 1i64;
+    let mut x = 2i64;
+
+    while x <= n as i64 {
+        if smallest % x != 0 {
+            let mut factor = 2;
+            loop {
+                if (smallest * factor) % x == 0 {
+                    break;
+                }
+                factor += 1;
+            }
+            smallest *= factor;
+        }
+        x += 1;
+    }
+
+    smallest
+}
+
+#[allow(dead_code)]
+pub fn euler5() {
+    let answer = smallest_multiple(20);
+    println!("Euler 5: {}", answer);
+}
--- hello-rust/hello-rust/src/main.rs
+++ hello-rust/hello-rust/src/main.rs
@@ -2,11 +2,13 @@ mod euler1;
 mod euler2;
 mod euler3;
 mod euler4;
+mod euler5;
 mod time;
 
 fn main() {
-    time!(euler1::euler1());
-    time!(euler2::euler2());
-    time!(euler3::euler3());
-    time!(euler4::euler4());
+    // time!(euler1::euler1());
+    // time!(euler2::euler2());
+    // time!(euler3::euler3());
+    // time!(euler4::euler4());
+    // time!(euler5::euler5());
 }
