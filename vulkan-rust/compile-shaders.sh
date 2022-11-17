284610ce56c9b36e23d7e5f4af4bd47ae954b09d
Brandon Slade <brandonyoyoslade@gmail.com>
2022-11-16 18:24:00 -0700

    feat(vulkan-rust): add static_tri shader and bash script to compile shaders

---
+++ vulkan-rust/compile-shaders.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+shopt -s extglob dotglob nullglob
+
+cd shaders
+for d in */ ; do
+    cd "$d"
+    for f in !(*.spv) ; do
+        echo "Compiling $d$f..."
+        glslc "$f" -o "$f.spv"
+    done
+done
